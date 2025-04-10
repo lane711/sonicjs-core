2c2de9c7f83010103d494d943c883dd10049f399
Lane Campbell <lane@sonicjs.com>
2025-04-10 14:58:55 -0700

    to esnext

--- index.ts
+++ index.ts
@@ -1,14 +1,1 @@
-/**
- * Core functionality for SonicJs, a headless CMS optimized for Cloudflare
- */
-
-/**
- * Echo function that returns the input value
- * @param input - The value to be returned
- * @returns The input value
- */
-export const echo = <T>(input: T): T => {
-    return input;
-};
-
-
+export * from "./src/echo";
